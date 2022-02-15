fe6d384a998374eaf858bf75f4a35b939e5de380
Blayne Mayfield <blayne.mayfield@okstate.edu>
2022-02-15 08:30:07 -0600

    Initial Commit

---
+++ Core Data Demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Core Data Demo
+//
+//  Created by Blayne Mayfield on 2/15/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
